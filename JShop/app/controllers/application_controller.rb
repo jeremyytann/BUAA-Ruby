fe12889df07cd45f80caa6df1503c5fb2589f5cb
Jeremy <tanchiachun2014@gmail.com>
2021-12-17 03:52:03 +0800

    done login logout

--- JShop/app/controllers/application_controller.rb
+++ JShop/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :role, :phone_number, :address, :email, :password, :remember_me])
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :remember_me])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :role, :phone_number, :address, :email, :password, :remember_me])
+    end
 end
